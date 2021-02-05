236a15e55af3b10f3bd5036059bde3e20bcbd8e4
Malcolm Macleod <malcolm@macleodhome.com>
2021-02-05 23:15:48 +0000

    3.22 - ESLint

--- phonebook/index.js
+++ phonebook/index.js
@@ -3,125 +3,118 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-const mongoose = require('mongoose')
-const uniqueValidator = require('mongoose-unique-validator')
 const PhoneBook = require('./models/phoneBook')
-
-
 
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
 app.use(morgan('tiny'))
 
+morgan.token('body', (req) => JSON.stringify(req.body))
 
-
-morgan.token('body', (req, res) => JSON.stringify(req.body))
-
-  app.get('/api/persons', (request, response) => {
-
+app.get('/api/persons', (request, response) => {
     PhoneBook.find({}).then(persons => {
-      response.json(persons)
+        response.json(persons)
     })
-  })
+})
 
-  app.get('/info', (request, response, error) => {
+app.get('/info', (request, response, next) => {
     let count = 0
     const time = new Date().toString()
 
     PhoneBook.find({}).then(persons => {
-      count = persons.length
-      response.send(`<p>Phonebook has info for ${count} people.</p><p>${time}</p>`)
+        count = persons.length
+        response.send(`<p>Phonebook has info for ${count} people.</p><p>${time}</p>`)
     })
-    .catch(error => next(error))
-  })
+        .catch(error => next(error))
+})
 
-  app.get('/api/persons/:id', (request, response, next) => {
+app.get('/api/persons/:id', (request, response, next) => {
     PhoneBook.findById(request.params.id)
-      .then(entry => {
-        if (entry) {
-          response.json(entry)
-        } else {
-          response.status(404).end()
-        }
-      })
-      .catch(error => next(error))
-  })
+        .then(entry => {
+            if (entry) {
+                response.json(entry)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
 
-  app.delete('/api/persons/:id', (request, response, next) => {
-     PhoneBook.findByIdAndRemove(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+app.delete('/api/persons/:id', (request, response, next) => {
+    PhoneBook.findByIdAndRemove(request.params.id)
+        .then(() => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
 
-  app.put('/api/persons/:id', (request, response, next) => {
+app.put('/api/persons/:id', (request, response, next) => {
     const body = request.body
 
     const entry = {
-      name: body.name,
-      number: body.number
+        name: body.name,
+        number: body.number
     }
-     PhoneBook.findByIdAndUpdate(request.params.id, entry, { new: true, runValidators: true })
-      .then(updatedEntry => {
-        response.json(updatedEntry).end()
-      })
-      .catch(error => next(error))
-  })
+    PhoneBook.findByIdAndUpdate(request.params.id, entry, { new: true, runValidators: true })
+        .then(updatedEntry => {
+            response.json(updatedEntry).end()
+        })
+        .catch(error => next(error))
+})
 
-  app.post('/api/persons', (request, response, next) => {
+app.post('/api/persons', (request, response, next) => {
 
     const body = request.body
     console.log(body)
 
     if (!body.name) {
-      return response.status(400).json({ 
-        error: 'name missing' 
-      })
+        return response.status(400).json({
+            error: 'name missing'
+        })
     }
 
     if (!body.number) {
-      return response.status(400).json({ 
-        error: 'number missing' 
-      })
+        return response.status(400).json({
+            error: 'number missing'
+        })
     }
 
     const phoneBook = new PhoneBook({
-      name: body.name,
-      number: body.number
+        name: body.name,
+        number: body.number
     })
 
     phoneBook.save().then(result => {
-      console.log(`added ${result.name} number ${result.number} to the database!`)
-      response.json(result).end()
+        console.log(`added ${result.name} number ${result.number} to the database!`)
+        response.json(result).end()
     })
-    .catch(error => next(error))
-  })
+        .catch(error => next(error))
+})
 
-  const unknownEndpoint = (request, response) => {
+const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
-  }
+}
 
-  // handler of requests with unknown endpoint
-  app.use(unknownEndpoint)
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
 
-  const errorHandler = (error, request, response, next) => {
+const errorHandler = (error, request, response, next) => {
     console.error(error.name)
 
     if (error.name === 'CastError') {
-      return response.status(400).send({ error: 'malformatted id' })
-    } 
+        return response.status(400).send({ error: 'malformatted id' })
+    }
     else if (error.name === 'ValidationError') {
-      return response.status(400).send({ error: error.message })
+        return response.status(400).send({ error: error.message })
     }
 
     next(error)
-  } 
+}
 
-  app.use(errorHandler)
+app.use(errorHandler)
 
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
+const PORT = process.env.PORT
+app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
-  })
+})
--- phonebook/mongo.js
+++ phonebook/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,44 +11,44 @@ const url =
   `mongodb+srv://fullstac-user:${password}@myfullstackopencluster.sqiz4.mongodb.net/phoneBook?retryWrites=true&w=majority&ssl=true`
 
 const phoneBookSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: String,
+    number: String
 })
 
 const PhoneBook = mongoose.model('PhoneBook', phoneBookSchema)
 
 const findPhonebookEntries = () => {
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-  PhoneBook
-  .find({})
-  .then(entries=> {
-    entries.forEach(entry => console.log(`${entry.name} ${entry.number}`))
-    mongoose.connection.close()
-  })
+    PhoneBook
+        .find({})
+        .then(entries => {
+            entries.forEach(entry => console.log(`${entry.name} ${entry.number}`))
+            mongoose.connection.close()
+        })
 }
 
 const addPhonebookEntry = (name, number) => {
-  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
-  const phoneBook = new PhoneBook({
-    name: name,
-    number: number
-  })
+    const phoneBook = new PhoneBook({
+        name: name,
+        number: number
+    })
 
-  phoneBook.save().then(result => {
-    console.log(`added ${result.name} number ${result.number} to the database!`)
-    mongoose.connection.close()
-  })
+    phoneBook.save().then(result => {
+        console.log(`added ${result.name} number ${result.number} to the database!`)
+        mongoose.connection.close()
+    })
 }
 
 if (process.argv.length === 3) {
-  findPhonebookEntries()
+    findPhonebookEntries()
 } else {
-  const name = process.argv[3]
+    const name = process.argv[3]
 
-  const number = process.argv[4]
+    const number = process.argv[4]
 
-  addPhonebookEntry(name, number)
+    addPhonebookEntry(name, number)
 }
 
